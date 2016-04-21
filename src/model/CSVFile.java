c1f813427e13c88c79bb129fb60c41396f4e4a8c
FranckeSelau <francke.selau@gmail.com>
2016-04-21 16:12:24 -0300

    trabalho 1.5

--- src/model/CSVFile.java
+++ src/model/CSVFile.java
@@ -4,37 +4,39 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class CSVFile {
-	
-	CSVParser<Paciente> paciente = new PacienteParser();
-	
-	/*private CSVParser<T> objectParser;	
-	public T setParser(Scanner leitor){
-		T elemento = objectParser.parseObject(leitor);
-		return elemento;
-	}*/
-	
-	public void openFile(){
-		try {
-			FileReader fr = new FileReader("paciente.csv");
-			Scanner leitor = new Scanner(fr);
-			leitor.useDelimiter("[,\n]");
-			while (leitor.hasNext()) {
-        		paciente.parseObject(leitor);
-        		System.out.println("---------------------------------------");
-        		System.out.println(paciente.toString());
-        	}
-       	   Close(leitor);
-		} catch (FileNotFoundException fnfe) {
-			System.err.println("ERRO! Arquivo não Encontrado");
-		}
-	}
-	public void readObject(Scanner leitor){
-	
-	}
-	public void Close(Scanner leitor){
-		leitor.close();
-	}
+public class CSVFile<T>{
+	private CSVParser<T> objectParser;
+	private Scanner leitor;
 
+    public void Open(String filename) {
+    	try{
+	    FileReader fr = new FileReader(filename);
+            leitor = new Scanner(fr);
+            leitor.useDelimiter("[\n]");
+        	while (leitor.hasNext()) {
+        		T iterador;
+        		iterador=readObject();
+        		System.out.println("---------------------------");
+        		System.out.println(iterador.toString());
+        	}
+       	   Close();
+ 	}catch (FileNotFoundException fnfe) {
+            System.err.println(fnfe.getMessage());
+        }
+    }
 	
+	public T readObject(){
+            T elemento = objectParser.parseObject(leitor.next());
+            return elemento;
+	}
+	
+	public void Close(){
+            leitor.close();
+	}
+	
+	public void setParser(CSVParser<T> parser){
+            this.objectParser=parser;
+	}
 }
+	
+
