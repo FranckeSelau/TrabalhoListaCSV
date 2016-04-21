3b222b6fa9cdd397bfdca0ee76b8303cf3cb2108
FranckeSelau <francke.selau@gmail.com>
2016-04-21 14:35:27 -0300

    trabalho 1.5

--- src/model/CSVFile.java
+++ src/model/CSVFile.java
@@ -4,24 +4,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class CSVFile<T> {
-	private CSVParser<T> objectParser;
+public class CSVFile {
 	
+	CSVParser<Paciente> paciente = new PacienteParser();
+	
+	/*private CSVParser<T> objectParser;	
 	public T setParser(Scanner leitor){
 		T elemento = objectParser.parseObject(leitor);
 		return elemento;
-	}
-
+	}*/
+	
 	public void openFile(){
 		try {
 			FileReader fr = new FileReader("paciente.csv");
 			Scanner leitor = new Scanner(fr);
 			leitor.useDelimiter("[,\n]");
 			while (leitor.hasNext()) {
-        		T iterador;
-        		iterador = setParser(leitor);
+        		paciente.parseObject(leitor);
         		System.out.println("---------------------------------------");
-        		System.out.println(iterador.toString());
+        		System.out.println(paciente.toString());
         	}
        	   Close(leitor);
 		} catch (FileNotFoundException fnfe) {
