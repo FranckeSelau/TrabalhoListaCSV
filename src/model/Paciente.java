5c6a156711cbf6f3ec86de94707471bc1085ff64
FranckeSelau <francke.selau@gmail.com>
2016-04-21 17:26:49 -0300

    trabalho 1.5

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -28,9 +28,10 @@ public class Paciente {
 	@Override	
 	public String toString() {
 		String data = dataNascimento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")); //converte a data ISO em data BR
-		return String.format("%-15s", "| RG: " + getRg()) + "\t"
-        + String.format("%-30s", "| NOME: " + getNome()) + "\t"
+		return String.format("%-20s", " RG: " + getRg()) + "\t"
+        + String.format("%-25s", "| NOME: " + getNome()) + "\t"
         + String.format("%-20s", "| DATA NASC.: " + data + "\n"
-        + "-----------------------------------------------------------\n");
+        + "-----------------------------------------"
+        + "-----------------------------------------");
 	}	
 }
